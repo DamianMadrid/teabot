86204742a63337d2cf089dab15e169d1077acb5a
teapot <teapot@kali.kali>
2021-11-21 04:37:05 +0000

    updated scraper and telegram bot

--- alerts.py
+++ alerts.py
@@ -1,8 +1,83 @@
-from dotenv.main import DotEnv
-from telegram.ext import Updater
-from dotenv import load_dotenv
+from telegram import Update, chatinvitelink, update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from dotenv import dotenv_values
+from subprocess import check_output
+from amazonScraper_rbpi import generate_Update
+from datetime import datetime
+import pickle
+import os
 
 
-token = load_dotenv('./env')
+config =dotenv_values('.env')
+TELEGRAM_TOKEN = config["TELEGRAM_TOKEN"]
+SUB_DATA_PATH = os.path.join(os.path.expanduser("~"),"YARAPT/data/subscriptions.csv")
 
-print(token)
+updater = Updater(TELEGRAM_TOKEN)
+dispatcher = updater.dispatcher
+active_subs = None
+
+def dumpSubList():
+    global active_subs
+    with open(SUB_DATA_PATH,"wb+") as fp:
+        pickle.dump(active_subs,fp)
+
+def loadSubList():
+    global active_subs
+    with open(SUB_DATA_PATH,"rb") as fp:
+        active_subs = pickle.load(fp)
+
+def sendUpdate(context:CallbackContext)->None:
+    update_str = generate_Update()
+    #update_str = "Testing"
+    if update_str:
+        job = context.job
+        context.bot.send_message(job.context,update_str)
+    else:
+        print(f"There was no update {datetime.now()}")
+
+
+
+def _subscribe(update:Update, context:CallbackContext)->None:
+    global active_subs
+    chat_id = update.message.chat_id
+    print(chat_id,type(chat_id))
+    print("Bot: new subscriber")
+    if chat_id in active_subs:
+        print("Already subbed")
+        update.message.reply_text("You're already subbed!")
+        return;
+    active_subs.append(chat_id)
+    dumpSubList()
+    context.job_queue.run_once(sendUpdate,5,context=chat_id,name=f"once:{chat_id}")
+    context.job_queue.run_repeating(sendUpdate,3600,context=chat_id,name=str(chat_id))
+    update.message.reply_text("Successfully subbed")
+
+
+def _whereAreYouCmmnd(update:Update, context:CallbackContext)->None:
+    ip_addr = check_output("curl -s ifconfig.me" ,shell=True)
+    ip_addr = ip_addr.decode("utf-8")
+    update.message.reply_text(ip_addr)
+
+def _Ping(update:Update, context:CallbackContext)->None:
+    update.message.reply_text("Bot back online")
+
+def _resumeSubscription():
+    for sub in active_subs:
+        print (f"Resuming sub with: {sub}")
+        print(type(sub))
+        updater.bot.sendMessage(sub,"Bot back online",)
+        dispatcher.job_queue.run_repeating(sendUpdate,3600,context=sub,name=str(sub),job_kwargs = {"max_instances":2,"misfire_grace_time":None})
+
+def main():
+    print("Bot startup")
+    loadSubList()
+    _resumeSubscription()
+    dispatcher.add_handler(CommandHandler("WhereAreYou", _whereAreYouCmmnd))
+    dispatcher.add_handler(CommandHandler("Subscribe", _subscribe))
+    updater.start_polling()
+    print("Bot listening")
+    updater.idle()
+
+if __name__ == '__main__':
+	main()
+
--- amazonScraper_rbpi.py
+++ amazonScraper_rbpi.py
@@ -1,21 +1,20 @@
 #Date and time
 import time
 from datetime import datetime
-import sys
 # Data scraping
 import bs4
+from bs4.element import ResultSet
 import requests
 # Data wrangling
-import numpy as np
 import pandas as pd
-from math import isnan, nan
-from collections import Counter
-from alerts import Mail_alert
+from math import isnan
+# Path 
+import os 
 
 
 # Requests headers
-hdrs = {
-    "User agent": "Mozilla/5.0 (Windows NT 10.0Win64x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+
+#hdrs = {"User agent": "Mozilla/5.0 (Windows NT 10.0Win64x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
 idDict = {
     'tab': 'my-lists-tab',
     'items': 'g-items',
@@ -23,44 +22,43 @@ idDict = {
     'end': 'endOfListMarker'}
 # Miscellaneous
 loading = ['/', '|', '\\']
-
-current_exec = str(datetime.now())
+current_exec = ""
 
 AMAZONURL = "https://www.amazon.com.mx"
 # PATH
-PATH_WHISHLISTS = "./whishlists.csv"
-PATH_PRICES_DATA = "./prices_data.csv"
+PATH_WHISHLISTS =os.path.join(os.path.expanduser("~"),"YARAPT/data/whishlists.csv")
+PATH_PRICES_DATA = os.path.join(os.path.expanduser("~"),"YARAPT/data/prices_data.csv")
 
 
-def awaitConnection():
-    i = 0
-    while(True):
+
+def awaitConnection()->bool:
+    for i in range(10):
         try:
-            requests.get(AMAZONURL, headers=hdrs, timeout=.2)
+            req_session.get(AMAZONURL, timeout=1)
+            return True
         except:
-            i = (i+1) % 3
             time.sleep(1)
             continue
         break
-    return
-
+    return False
 
 meta_col = "url,name,price,goal,category,last_seen,lowest,highest".split(",")
-
+req_session = requests.Session()
 
 def getPage(url: str):
+    global req_session
     try:
-        page = requests.get(url, headers=hdrs)
+        page = req_session.get(url)
         soup = bs4.BeautifulSoup(page.content, 'html.parser')
         return soup
     except requests.ConnectionError:
-        print('Could not connect. Try again when connected')
-        sys.exit()
+        return
     return
 
-
 def isEndOfList(page):
-    if(page.find(id=idDict['end'])):
+    is_end = page.find(id=idDict['end'])
+    #print("is end:",is_end)
+    if(is_end):
         return True
     return False
 
@@ -72,34 +70,43 @@ def retrieveAmazonData(wl_url, path):
     df: pd.DataFrame = pd.read_csv(path, index_col="url")
     currBlock = wl_url
     while(True):
+        time.sleep(1)
         # Retrieve wishlist
         page = getPage(AMAZONURL+currBlock)
+        # print(currBlock)
+        if page == None:
+            #print("page is ",  page)
+            return False
+        #print("page returned not none")
         whishlist = page.find(id=idDict['items'])
-        if whishlist == None:
-            print("Cannot get items from whishlist")
-            print(page)
-            sys.exit()
-            return
-        for child in whishlist.find_all("li"):
-            priceData = child.find('span', {"class": "a-offscreen"})
-            if priceData != None:
-                title_tag = child.find('h3').find('a')
-                price = priceData.contents[0][1:].replace(',', '')
-                url = title_tag['href'][0:14]
-                df.at[url, current_exec] = price
-                df.at[url, "name"] = title_tag['title'][0:20]
+        #print(whishlist)
+        if whishlist != None:
+            for child in whishlist.find_all("li"):
+                priceData = child.find('span', {"class": "a-offscreen"})
+                if priceData != None:
+                    title_tag = child.find('h3').find('a')
+                    price = priceData.contents[0][1:].replace(',', '')
+                    url = title_tag['href'][0:14]
+                    df.at[url, current_exec] = price
+                    df.at[url, "name"] = title_tag['title'][0:20]
         # Check for end of list
         if(not isEndOfList(page)):
-            currBlock = page.find("a", class_=idDict['load_more'])['href']
+            currBlock = page.find("a", class_=idDict['load_more'])
+            try:
+                currBlock = currBlock['href']
+            except:
+                print(page)
+                return False
         else:
             break
     # Save data
     df.to_csv(PATH_PRICES_DATA, index="url")
-    return
+    return True
 
 
-def dataInfo(path, save=False):
+def dataInfo(path, save=False)->str:
     global current_exec
+    alert_message = {}
     # Setting the plot props
     df: pd.DataFrame = pd.read_csv(
         path, index_col="url", dtype={'last_seen': 'object'})
@@ -107,44 +114,56 @@ def dataInfo(path, save=False):
         current_price = data.at[current_exec]
         if not isnan(current_price):
             # General update
-            alerter.addToAlert(url, data['name'])
+            alert_message[url] = {"name":data['name'],"content":""}
             df.at[url, "last_seen"] = current_exec
             if data.lowest > current_price or pd.isna(data.lowest):
                 df.at[url, "lowest"] = current_price
-                alerter.addContent(url,
-                                   f"(LOWEST) is at: {current_price}!\n")
+                alert_message[url]["content"]= f"⏬{current_price:.2f}\n"
+            # Check if available again
+            if pd.isna(data["last_seen"]):
+                alert_message[url]["content"]+= f"📦{current_price:.2f}\n"
             if data.highest < current_price or pd.isna(data.highest):
                 df.at[url, "highest"] = current_price
             if data.goal > current_price:
-                alerter.addToAlert(
-                    f"(GOAL) is at: {current_price}!\n")
-            # Check if available again
-            if pd.isna(data["last_seen"]):
-                alerter.addToAlert(
-                    f"(RESTOCK) is available again: {current_price}\n")
+                alert_message[url]["content"]= f"🏁{current_price:.2f}\n"
             # Calculate change in price
             else:
-                last_price = data[data["last_seen"]]
-                priceDelta = last_price - current_price
-                is_disccout = priceDelta > 0
-                priceDelta = abs(priceDelta)
-                # Bigger than 10%
-                if priceDelta > last_price * .1:
-                    alerter.addToAlert(
-                        f"Price is {'DOWN' if is_disccout else 'UP'} by {priceDelta}\n")
-    df.to_csv("prices_data.csv")
+                if pd.notna(data["last_seen"]): 
+                    last_price = data[data["last_seen"]]
+                    priceDelta = last_price - current_price
+                    is_disccout = priceDelta > 0
+                    priceDelta = abs(priceDelta)
+                    # Bigger than 10%
+                    if priceDelta > last_price * .1:
+                        alert_message[url]["content"]+= f"{'🟢' if is_disccout else '🔴'} {last_price} -> {current_price:.2f}\n"
+    df.to_csv(PATH_PRICES_DATA)
+    final_message = ''
+    for key, val in alert_message.items():
+        if val["content"]!="":
+            final_message+=f'{val["name"]}\n     {val["content"]}'
+    return final_message
 
 
-if __name__ == '__main__':
-    global alerter
-    alerter = Mail_alert()
-    # Check for connection
-    awaitConnection()
+
+def generate_Update():
+    global current_exec
+    current_exec = str(datetime.now())
+    if not awaitConnection():
+        return "await connection error"
     # Get whislist list
     wishlists_df = pd.read_csv(PATH_WHISHLISTS, index_col="url")
     # Start scrapping
     for url, name in wishlists_df.iterrows():
-        # time.sleep(5)
-        retrieveAmazonData(url, PATH_PRICES_DATA)
-    dataInfo(PATH_PRICES_DATA)
-    alerter.printAlerts()
+        if not retrieveAmazonData(url, PATH_PRICES_DATA):
+            return f"cannot retrieve {name} "
+    return dataInfo(PATH_PRICES_DATA)
+
+
+def main():
+    print("This scripts is not supposed to be executed unless you wan't to try something outside the bot itself")
+    print("Entering into debugging mode")
+    test_str = generate_Update()
+    print(test_str)
+
+if __name__ == "__main__":
+    main()
