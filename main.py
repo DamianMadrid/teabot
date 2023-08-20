9c2d417d7fde7e530b676f5621c85a28929b1db9
Damian <asdf1234damian@gmail.com>
2023-08-20 00:16:25 -0600

    Refactoring Added ScrapperApp as the main class Redefined commands TODO : default handler

--- main.py
+++ main.py
@@ -3,94 +3,102 @@ from telegram.ext import (Application,CommandHandler,ContextTypes,CallbackContex
 from dotenv import dotenv_values
 from subprocess import check_output
 from scrapper import Scrapper
-from datetime import datetime
+import pendulum
 import os
 
-config =dotenv_values('.env')
-TELEGRAM_TOKEN = config["TELEGRAM_TOKEN"]
-DATA_PATH = os.path.expanduser("./data/")
-SUB_DATA_PATH = DATA_PATH+"subscriptions.pickle"
-application = Application.builder().token(TELEGRAM_TOKEN).build()
-main_scrapper = Scrapper(DATA_PATH)
+TELEGRAM_TOKEN = dotenv_values('.env')["TELEGRAM_TOKEN"]
+DATA_DIRECTORY = os.path.expanduser("./data/")
 
-async def _sendUpdate(context:ContextTypes.DEFAULT_TYPE)->None:
-    job = context.job
-    main_scrapper.updateSavedProd(str(datetime.now().date()))
-    update_str = main_scrapper.updateString(str(datetime.now().date()))
-    if update_str:
-        await context.bot.send_message(job.chat_id,update_str,parse_mode="HTML")
-    else:
-#        await context.bot.send_message(job.chat_id,f"No update {str(datetime.now().date())}",parse_mode="HTML")
-        print(f"There was no update {datetime.now()}")
+class ScrapperApp:
 
-async def _forceUpdate(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    chat_id = update.effective_message.chat_id
-    try:
-        context.job_queue.run_repeating(_sendUpdate,interval=3600,first=5, chat_id=chat_id, name=str(chat_id))
-    finally:
-        pass
-    return
+    async def _scheduledUpdate(self,context:ContextTypes.DEFAULT_TYPE)->None:
+        job = context.job
+        
+        current_date= str(pendulum.now().date())
+        self.scrapper.updateSavedProd(current_date)
+        update_str = self.scrapper.updateString(current_date)
+        if update_str:
+            print("sending update string: ",update_str)
+            await context.bot.send_message(job.chat_id,update_str,parse_mode="HTML")
+        else:
+            print(f"There was no update {pendulum.now()}")
+
+    async def _getIP(self,update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
+        ip_addr = check_output("curl -s ifconfig.me" ,shell=True)
+        ip_addr = ip_addr.decode("utf-8")
+        await update.message.reply_text(ip_addr)
+
+    def _isSubscribed(self,update:Update, context:ContextTypes.DEFAULT_TYPE)->bool:
+        chat_id = str(update.message.chat_id)
+        chat_jobs = context.job_queue.get_jobs_by_name(chat_id)
+        return len(chat_jobs)
 
 
-async def _getIP(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    ip_addr = check_output("curl -s ifconfig.me" ,shell=True)
-    ip_addr = ip_addr.decode("utf-8")
-    await update.message.reply_text(ip_addr)
+    async def _subscribe(self,update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
+        if self._isSubscribed(update=update,context=context):
+            await update.message.reply_text("You're already subscribed")
+            return 
+        _chat_id = str(update.message.chat_id)
+        context.job_queue.run_repeating(self._scheduledUpdate,first=0,interval=3600,name=_chat_id)    
+        await update.message.reply_text("Succesfully subscribed")
 
-async def _Ping(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    await update.message.reply_text("Bot back online")
+    async def _nextUpdate(self,update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
+        _chat_id = str(update.message.chat_id)
+        if self._isSubscribed(update,context):
+            next_job= context.job_queue.get_jobs_by_name(_chat_id)[0]
+            _time_delta = next_job.next_t - pendulum.now()
+            await update.message.reply_text(f'Next update scheduled in {_time_delta.in_minutes()} minutes')
+            return
+        await update.message.reply_text("Not subscribed")
 
-async def _addProd(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    command_args = update.message.text.split(" ")[1:]
-    if len(command_args) in [1,2]:
-        if len(command_args) == 2:
-            try:
-                float(command_args[1])
-                add_result = main_scrapper.addProd(command_args[0],command_args[1])
-                await update.message.reply_text("Product Added succesfully" if add_result else "Failed to add")
-                return
-            except:
-                await update.message.reply_text("Goal not valid")
-                return
-        add_result = main_scrapper.addProd(command_args[0])
-        await update.message.reply_text("Product added succesfully" if add_result else "Failed to add")
+    async def _defaultHandler(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
+        #TODO
+        #check if is a valid product url 
+        #if so add as product
+        #add a reply for feedback
+        pass 
+
+
+    async def _addProduct(self,update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
+        _message = update.message.text
+        prodURL = _message.split()[1]
+        add_result = self.scrapper.addProd(prodURL)
+        await update.message.reply_text("Product Added succesfully" if add_result else "Failed to add")
         return
-    else:
-        await update.message.reply_text("Args are /addProd url|str (goal|float)")
+        
+
+    def _getPersistance(self,data_directory:str):
+        if not os.path.exists(f"{data_directory}persistance.pckl"):
+            print("No persistance found")
+            return None
+        return PicklePersistence(filepath=f"{data_directory}persistance.pckl")
+    
+    def _initTelegramApp(self):
+        _persistence = self._getPersistance(DATA_DIRECTORY)
+        self.telegramApp = Application.builder().token(TELEGRAM_TOKEN)
+        if _persistence is None:
+            self.telegramApp = self.telegramApp.persistence(_persistence)
+        self.telegramApp = self.telegramApp.build()
+        self.telegramApp.add_handler(CommandHandler("get_ip", self._getIP))
+        self.telegramApp.add_handler(CommandHandler("next_update", self._nextUpdate))
+        self.telegramApp.add_handler(CommandHandler("subscribe", self._subscribe))
+        self.telegramApp.add_handler(CommandHandler("add", self._addProduct))
+        self.telegramApp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self._defaultHandler))
+        self.telegramApp.run_polling()
         return
 
-async def _checkWishlists(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    result = main_scrapper.checkWishList()
-    await update.message.reply_text(result)
-    return
-
-async def _removeProd(update:Update, context:ContextTypes.DEFAULT_TYPE)->None:
-    command_args = update.message.text.split(" ")[1:]
-    if len(command_args) ==1:
-        remove_result =  main_scrapper.removeProd(command_args[0])
-        await update.message.reply_text("Product removed succesfully" if remove_result else "Failed to remove")
+    def _initScrapper(self):
+        self.scrapper = Scrapper(DATA_DIRECTORY)
         return
-    else:
-        await update.message.reply_text("Args are /addProd url|str")
-        return
+        
+    def __init__(self):    
+        self._initScrapper()
+        self._initTelegramApp()
 
-async def _general(update: Update, context: ContextTypes.DEFAULT_TYPE)->None:
-    await update.message.reply_text(update.message.text)
-    return
-
-
+        
 
 def main():
-    persistence = PicklePersistence(filepath="./data/persistance.pckl")
-    application = Application.builder().token(TELEGRAM_TOKEN).persistence(persistence).build()
-    application.add_handler(CommandHandler("getIP", _getIP))
-    application.add_handler(CommandHandler("update", _forceUpdate))
-    # application.add_handler(CommandHandler("addprod", _addProd))
-    # application.add_handler(CommandHandler("removeprod", _removeProd))
-    application.add_handler(CommandHandler("checkwishlists", _checkWishlists))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, _general))
-    print("Application started succesfully");
-    application.run_polling()
+    myApp = ScrapperApp()
 
 if __name__ == '__main__':
-        main()
+    main()
